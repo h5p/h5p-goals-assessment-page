c2868d681006996d22a0cf4cfe556b289cb43daa
Vilde Stabell <vilde.voss.stabell@gmail.com>
2024-12-11 15:52:26 +0100

    Remove label on help button

--- goals-assessment-page.js
+++ goals-assessment-page.js
@@ -165,7 +165,7 @@ H5P.GoalsAssessmentPage = (function ($, EventDispatcher) {
     if (self.params.helpText !== undefined && self.params.helpText.length !== 0) {
       self.$helpButton = $('<button>', {
         'class': 'page-help-text',
-        html: self.params.helpTextLabel,
+        'aria-label': self.params.helpTextLabel,
         click: function () {
           self.trigger('open-help-dialog', {
             title: self.params.title,
@@ -174,6 +174,7 @@ H5P.GoalsAssessmentPage = (function ($, EventDispatcher) {
         },
         appendTo: self.$pageTitle
       });
+      H5P.Tooltip(self.$helpButton[0]);
     }
 
     $('<div>', {
